b43f74fe1bcc36920e9e8fdbce08459c476270cb
Anuroopa-123 <128488430+Anuroopa-123@users.noreply.github.com>
2025-06-24 13:00:33 +0530

    Added Labs Section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { LabscontentComponent } from './labscontent/labscontent.component';
+import { MenulabsComponent } from './menulabs/menulabs.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'aboutus', component: AboutusComponent },
+  { path: 'labs/:key', component: MenulabsComponent },
 ];

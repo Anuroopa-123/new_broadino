f7db986126761ac4dbe9edc61137783b107d036a
Anuroopa-123 <128488430+Anuroopa-123@users.noreply.github.com>
2025-06-24 13:18:17 +0530

    Added contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { LabscontentComponent } from './labscontent/labscontent.component';
 import { MenulabsComponent } from './menulabs/menulabs.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'aboutus', component: AboutusComponent },
   { path: 'labs/:key', component: MenulabsComponent },
+  { path: 'contact_us', component: ContactComponent },
 ];

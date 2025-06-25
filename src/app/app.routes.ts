485a8f0d719ed5c6fd57975a62239201c141eb78
AravindhshankarTansam <aravindhshankar@tansam.org>
2025-06-26 00:23:43 +0530

    STEM work done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LabscontentComponent } from './labscontent/labscontent.component';
 import { MenulabsComponent } from './menulabs/menulabs.component';
 import { ContactComponent } from './contact/contact.component';
 import { MediapageComponent } from './mediapage/mediapage.component';
+import { STEMBrochureComponent } from './stem-brochure/stem-brochure.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'labs/:labKey', component: MenulabsComponent },
   { path: 'contact_us', component: ContactComponent },
   { path: 'labs', component: MenulabsComponent },
+  { path: 'stem', component: STEMBrochureComponent},
   { path: 'media', component: MediapageComponent },
 ];
---
+++ src/app/stem-brochure/stem-brochure.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stem-brochure',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stem-brochure.component.html',
+  styleUrls: ['./stem-brochure.component.css']
+})
+export class STEMBrochureComponent {
+
+}
